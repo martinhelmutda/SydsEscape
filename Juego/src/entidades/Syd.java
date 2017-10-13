14e2b82dcf489c8b4734803556b1e33a16165952
Unknown <martinhelmutmh@gmail.com>
2017-10-13 14:38:18 -0500

    Arreglo de Syd
    Ahora hay descripción, para que sea más fácili comprender lo que está sucediendo y los saltos son más rápidos y ágiles

--- Juego/src/entidades/Syd.java
+++ Juego/src/entidades/Syd.java
@@ -14,11 +14,11 @@ public class Syd extends Entidad{
 	private boolean right=false, left=false;
 	private boolean jump=false, fall=false; 
 
-	private double jumpSpeed = 5;
+	private double jumpSpeed = 17;
 	private double currentJumpSpeed=jumpSpeed;
 	
-	private double maxFallSpeed = 5;
-	private double currentFallSpeed = 0.1;
+	private double maxFallSpeed = 15;//esta es la velocidad maxima de caída que puede tener
+	private double currentFallSpeed = 2;//al llegar al punto mas alto regresara con una aceleracion que comienxa con currentFallSpeed y termina con maxFallSpeed
 	
 	public Syd(int width, int height) {
 		x=GamePanel.PWIDTH-160;
@@ -26,51 +26,65 @@ public class Syd extends Entidad{
 		this.width = width;
 		this.height = height;
 		
-		this.hitbox = new Rectangle(x,y,width,height);
+		this.hitbox = new Rectangle(x,y,width,height);//declaramos el área de golpe
 	};
 	
-	public void tick() {
-		if(right) {
-			x+=5;
-			if(x<=5) {
-				x=GamePanel.PWIDTH-5;
-			}else if(x>=GamePanel.PWIDTH-5) {
-				x=5;
+	public void tick() { //constantemente se estara leyendo la tecla que se presiona. Este metodo es responsable de cambiar el objeto de lugar
+		
+		if(fall||jump) { //Si está saltando o cayendo no podemos darle mucha velocidad en x porque hara un gran parabola
+			if(right) {
+				x+=3;
+				if(x>=GamePanel.PWIDTH-5) x=5;//Si la coordenada x está próxima al fin del Panel, enronces regresa por las coordenada x=5
+				
 			}
-		}
-		if(left) {
-			x-=5;
-			if(x<=5) {
-				x=GamePanel.PWIDTH-5;
-			}else if(x>=GamePanel.PWIDTH-5) {
-				x=5;
+			else if(left) {
+				x-=3; //la figura se desplaza 5 pixeles a la izquierda cada que se detecta la presión de la tecla
+				if(x<=5) x=GamePanel.PWIDTH-5;//Si la figura se aproxima al límite por el extremo izquierdo, continua su recorrido por el extremo derecho de la pantalla
 			}
-		}
-		if(jump) {
-			//Agregamos la gravedad
-			y -= currentJumpSpeed;
-			currentJumpSpeed -= .1;
 			
-			if (currentJumpSpeed <=0) {
-				currentJumpSpeed=jumpSpeed;
-				jump=false;
-				fall=true;
+			if(jump) {
+			//Agregamos la gravedad
+			//Recordemos que el extremo superior izquierdo es 0,0 entonces si queremos subir, debemos restar
+				y -= currentJumpSpeed; //El salto será de los pixeles que currentJumpSpeed indique
+				currentJumpSpeed -= 0.5;//currentJumpSpeed estara disminuyendo su valor cada que el render pase por aqui hasta que finalmente llegue a 0, el maximo de altura
+				
+				if (currentJumpSpeed <=0) { //Si ya llego a la maxima altura
+					currentJumpSpeed=jumpSpeed; //La caida comienza desde el valor inicial de caida
+					jump=false; //El salto deja de correr 
+					fall=true; //comienza a caer
+				}
+			}
+			
+			if(fall) { //Si ya está en estado de caída. 
+				if((int)y<GamePanel.PHEIGHT-165) {//EL suelo del juego es de 160 pixeles, por lo tanto es la altura del panel menos los 160
+					
+					y += currentFallSpeed; //el valor de y se estará incrementando (recordemos que incrementar es bajar) 
+					if(currentFallSpeed<maxFallSpeed) {//Si el valor de caida es menor al maximo 
+						currentFallSpeed+=0.5;//Se puede ir acelerando
+					}
+				}else {
+					fall=false;//Una vez que alcanza el suelo deja de caer
+				}
+				
 			}
 		}
 		
-		if(fall) {
-			y += currentFallSpeed;
-			if((int)y>=GamePanel.PHEIGHT-160) {
-				fall=false;
+		
+		if(!fall) { //Si ya no esta en estado de caida, el valor inicial regresa a .1
+			currentFallSpeed=0.5;
+		}
+		if(!jump&&!fall) { //Si no esta saltando ni cayendo la velocidad debe ser alta
+			if(right) {
+				x+=7;
+				if(x>=GamePanel.PWIDTH-5) x=5;//Si la coordenada x está próxima al fin del Panel, enronces regresa por las coordenada x=5
+				
 			}
-			if(currentFallSpeed<maxFallSpeed) {
-				currentFallSpeed+=.1;
+			else if(left) {
+				x-=7; //la figura se desplaza 5 pixeles a la izquierda cada que se detecta la presión de la tecla
+				if(x<=5) x=GamePanel.PWIDTH-5;//Si la figura se aproxima al límite por el extremo izquierdo, continua su recorrido por el extremo derecho de la pantalla
 			}
 		}
-		if(!fall) {
-			currentFallSpeed=.1;
-		}
-		hitbox.setLocation(x,y);
+		hitbox.setLocation(x,y);//el área de golpe no cambia, por lo tanto solo debemos ir ajustando las coordenadas en donde se encuentra nuestro objeto
 	}
 	
 	public void pinturita(Graphics dbg) {
--- Juego/src/interfaz/Main.java
+++ Juego/src/interfaz/Main.java
@@ -10,8 +10,8 @@ public class Main {
 		principal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		principal.setVisible(true);
 		principal.setLayout(new BorderLayout());
-		principal.setResizable(false);	//No queremos que vean lo que pasa detrás de cámaras
-		principal.pack();	//Redimensiona al tamaño del panel. Obtiene las dimensiones de cada elemento dentro del frame
+		principal.setResizable(false);	//No queremos que vean lo que pasa detras de camaras
+		principal.pack();	//Redimensiona al tamanio del panel. Obtiene las dimensiones de cada elemento dentro del frame
 		principal.setLocationRelativeTo(null);
 	}
 }
